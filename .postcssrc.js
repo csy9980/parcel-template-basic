153fbfcc80f5603f38132ddd831b8f2c24d6ba01
csy9980 <csy9980@gmail.com>
2024-07-03 23:45:27 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,24 @@
+// ESM 가져오기.내보내기
+// import autoprefixer from 'autoprefixer'
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+// CommonJS 가져오기.내보내기
+// const autoprefixer = require('autoprefixer')
+
+// module.exports = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+
+// 간소화
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
